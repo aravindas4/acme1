fad1d83f1731863ccb1cbb92f2f20e5abf2b7baf
aravindas4 <ash.g.education@gmail.com>
2020-07-31 12:34:21 +0530

    Add changes to product app models

--- product/models.py
+++ product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 
-from .tasks import load_products
+# from .tasks import load_products
 
 
 class Base(TimeStampedModel):
@@ -10,23 +10,11 @@ class Base(TimeStampedModel):
         abstract = True
 
 
-class FileProductUpload(Base):
-    document = models.CharField(max_length=255)
-
-    class Meta:
-        ordering = ('-created',)
-
-    def __str__(self):
-        return f'{self.document}'
-
-    def load_products(self):
-        load_products.delay(self.document, Product)
-
-
 class Product(Base):
     name = models.CharField(max_length=255)
     sku = models.CharField(max_length=255, unique=True)
     description = models.TextField()
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ('-created',)
