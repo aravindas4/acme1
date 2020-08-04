e72db7dbeaeb5b30d98042f80126d430978d813c
aravindas4 <ash.g.education@gmail.com>
2020-08-04 07:09:26 +0530

    Update celery config

--- acme/celery.py
+++ acme/celery.py
@@ -5,12 +5,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'acme.settings')
 
-app = Celery('acme', broker='amqp://myuser:mypassword@localhost/myvhost')
+app = Celery('acme', broker='pyamqp://localhost//')
 
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
