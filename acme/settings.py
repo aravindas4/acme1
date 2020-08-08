ec42b1ed39e67ebccb00d3228f6843834cdd1dbe
aravindas4 <ash.g.education@gmail.com>
2020-08-08 10:43:29 +0530

    Update settings file

--- acme/settings.py
+++ acme/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0']
 
 
 # Application definition
