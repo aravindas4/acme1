4edc2ff0871de221cf13ae03cf19e3932b775637
aravindas4 <ash.g.education@gmail.com>
2020-08-08 12:53:19 +0530

    Enable debug mode

--- acme/settings.py
+++ acme/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', '0.0.0.0']
 
